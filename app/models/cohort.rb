afa81394d4b2d0d36c0d06fe9a2aec5d49354355
Cameron <camcreel@gmail.com>
2021-08-01 16:39:28 -0400

    Completed assignment

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,3 +1,22 @@
 class Cohort < ActiveRecord::Base
-  
+    has_many :students
+    def add_student(name:,age:)
+        Student.create(name:name,age:age,cohort_id:self.id)
+    end
+
+    def average_age
+        self.students.average(:age)
+    end
+
+    def total_students
+        self.students.size
+    end
+
+    def self.sort_by_mod
+        self.order("current_mod")
+    end
+
+    def self.biggest
+        Cohort.joins(:students).group(:cohort_id).order("COUNT(cohort_id)  DESC").limit(1)[0]
+    end
 end
